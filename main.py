06fe28647b7839c9d6a672efe0eac4991a0f90f2
jinnosukeKato <jinnosukeKato@gmail.com>
2025-10-14 17:15:06 +0900

    小数に対応

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ from lark import Lark, Transformer
 
 class Eval(Transformer):
     # それぞれのルール名に関数が対応
-    def sum(self, values) -> int:
-        result = int(values[0])
+    def sum(self, values) -> float:
+        result = float(values[0])
         # [2::2]でvalue[2]から2個刻みのスライスができる ex) 2, 4, 6, ...
         # これにより，+を飛ばす
         for op, value in zip(values[1::2], values[2::2]):
             if op == "+":
-                result += int(value)
+                result += float(value)
             else:
-                result -= int(value)
+                result -= float(value)
         return result
 
-    def mul(self, values) -> int:
-        result = int(values[0])
+    def mul(self, values) -> float:
+        result = float(values[0])
         for op, value in zip(values[1::2], values[2::2]):
             if op == "*":
-                result *= int(value)
+                result *= float(value)
             else:
-                result //= int(value)
+                result /= float(value)
         return result
 
 
 with open("./calculator.lark", encoding="utf-8") as grammar:
     parser = Lark(grammar.read(), start="expr")
 
-tree = parser.parse("5*(2-4) / 5=")
+tree = parser.parse("2.5*2=")
 print(tree)
 print(tree.pretty())
 print(Eval().transform(tree))
