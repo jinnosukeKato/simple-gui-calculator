d4f5d65afbec7b9b7e744c531caa59214f1b2920
jinnosukeKato <jinnosukeKato@gmail.com>
2025-10-14 17:07:44 +0900

    四則演算ができるようになった

--- main.py
+++ main.py
@@ -7,21 +7,27 @@ class Eval(Transformer):
         result = int(values[0])
         # [2::2]でvalue[2]から2個刻みのスライスができる ex) 2, 4, 6, ...
         # これにより，+を飛ばす
-        for i in values[2::2]:
-            result += int(i)
+        for op, value in zip(values[1::2], values[2::2]):
+            if op == "+":
+                result += int(value)
+            else:
+                result -= int(value)
         return result
 
     def mul(self, values) -> int:
         result = int(values[0])
-        for i in values[2::2]:
-            result *= int(i)
+        for op, value in zip(values[1::2], values[2::2]):
+            if op == "*":
+                result *= int(value)
+            else:
+                result //= int(value)
         return result
 
 
 with open("./calculator.lark", encoding="utf-8") as grammar:
-    parser = Lark(grammar.read(), start="start")
+    parser = Lark(grammar.read(), start="expr")
 
-tree = parser.parse("5*(2+4)=")
+tree = parser.parse("5*(2-4) / 5=")
 print(tree)
 print(tree.pretty())
 print(Eval().transform(tree))
